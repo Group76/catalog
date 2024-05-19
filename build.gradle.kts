71c5375fe49acf5b65454870952063ee945e2f15
t-tozatto <thais.c.tozatto@gmail.com>
2024-05-18 23:44:52 -0300

    test detekt

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.DetektCreateBaselineTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 val jacocoExclusions = arrayOf(
@@ -122,4 +124,28 @@ tasks.withType<JacocoCoverageVerification> {
 			}
 		}))
 	}
+}
+
+detekt {
+	buildUponDefaultConfig = true // preconfigure defaults
+	allRules = true // activate all available (even unstable) rules.
+//	config.setFrom("$projectDir/config/detekt.yml") // point to your custom config defining rules to run, overwriting default behavior
+//	baseline = file("$projectDir/config/baseline.xml") // a way of suppressing issues before introducing detekt
+}
+
+tasks.withType<Detekt>().configureEach {
+	reports {
+		html.required.set(true) // observe findings in your browser with structure and code snippets
+		xml.required.set(true) // checkstyle like format mainly for integrations like Jenkins
+		txt.required.set(true) // similar to the console output, contains issue signature to manually edit baseline files
+		sarif.required.set(true) // standardized SARIF format (https://sarifweb.azurewebsites.net/) to support integrations with GitHub Code Scanning
+		md.required.set(true) // simple Markdown format
+	}
+}
+
+tasks.withType<Detekt>().configureEach {
+	jvmTarget = "1.8"
+}
+tasks.withType<DetektCreateBaselineTask>().configureEach {
+	jvmTarget = "1.8"
 }
