5473f5f2950d2bb487e1335a39d17d1ef53fd863
t-tozatto <thais.c.tozatto@gmail.com>
2024-06-12 07:41:36 -0300

    fix app prod yaml

--- src/main/kotlin/com/group76/catalog/CatalogApplication.kt
+++ src/main/kotlin/com/group76/catalog/CatalogApplication.kt
@@ -1,6 +1,6 @@
 package com.group76.catalog
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
@@ -13,9 +13,12 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @ConfigurationPropertiesScan("com.group76.catalog")
 @ComponentScan("com.group76.catalog")
 @SpringBootApplication(exclude = [DataSourceAutoConfiguration::class])
-@EnableAutoConfiguration
 class CatalogApplication
 
+@Value("\${mongoUri}")
+private val mongoUri: String? = null
+
 fun main(args: Array<String>) {
+	System.out.println("Param value is " + mongoUri);
 	runApplication<CatalogApplication>(*args)
 }
